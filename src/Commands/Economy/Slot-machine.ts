f7987a67443e82ea7ce2814a20e0431d486be52a
Kakashi hatake <105074780+kakashuuu@users.noreply.github.com>
2024-03-28 14:56:07 +0530

    Update Slot-machine.ts

--- src/Commands/Economy/Slot-machine.ts
+++ src/Commands/Economy/Slot-machine.ts
@@ -1,33 +1,32 @@
-import { SlotMachine, SlotSymbol } from 'slot-machine'
-import { BaseCommand, Command, Message } from '../../Structures'
+import { BaseCommand, Command, Message } from '../../Structures';
+import { SlotMachine, SlotSymbol } from 'slot-machine';
 
 @Command('slot', {
     category: 'economy',
     description: 'Bets the given amount of gold in a slot machine',
     usage: 'slot <amount>',
-    cooldown: 35,
+    cooldown: 7,
+    casino: true,
     exp: 10,
     aliases: ['bet']
 })
-export default class command extends BaseCommand {
-    override execute = async (M: Message): Promise<void> => {
-        if (M.numbers.length < 1) return void M.reply(`amount?`)
-        const amount = M.numbers[0];
-        const { wallet } = await this.client.DB.getUser(M.sender.jid)
-        if (amount > wallet) return void M.reply(`check ur wallet`)
-        const machine = new SlotMachine(3, this.symbols)
-        const results = machine.play();
-        const lines = results.lines.filter((line) => !line.diagonal);
-        const points = results.lines.reduce((total, line) => total + line.points, 0)
-        const resultAmount = points <= 0 ? -amount : amount * points;
-        await this.client.DB.setGold(M.sender.jid, resultAmount)
+export default class SlotCommand extends BaseCommand {
+    private thumbnailUrls: string[] = [
+        'https://telegra.ph/file/2fa16c9937561cedb2149.jpg',
+        'https://telegra.ph/file/bcfccd97a8fb0f2de97b7.jpg',
+        'https://telegra.ph/file/2a8833584499f8ab964eb.jpg',
+        'https://telegra.ph/file/dd4e9d5edfe8e321eb2cd.jpg',
+        'https://telegra.ph/file/3ae35aa1970b951cc1a07.jpg',
+    ];
 
-        let text = '🎰 *SLOT MACHINE* 🎰\n\n';
-        text += results.visualize();
-        text += points <= 0 ? `📉 You lost ${amount} gold` : `📈 You won ${resultAmount} gold`;
+    private getRandomThumbnailUrl(): string {
+        const randomIndex = Math.floor(Math.random() * this.thumbnailUrls.length);
+        return this.thumbnailUrls[randomIndex];
+    }
 
-        return void (await this.client.sendMessage(M.from, { text }, { quoted: M.message }))
-    };
+    private betsCount = 0;
+    private jackpotThreshold = 20;
+    private jackpotMultiplier = 40;
 
     private symbols = [
         new SlotSymbol('1', {
@@ -46,5 +45,50 @@ export default class command extends BaseCommand {
             weight: 40
         })
     ];
-                                        }
-                                                                      
+
+    override execute = async (M: Message): Promise<void> => {
+        if (M.numbers.length < 1) return void M.reply(`amount?`)
+
+        const amount = M.numbers[0]
+        const { credits } = await this.client.DB.getUser(M.sender.jid)
+
+        if (amount < 100)
+            return void M.reply(`🟨 The minimum amount for betting is *100*`)
+
+        if (amount > credits)
+            return void M.reply(`🟥 *You need ${amount - credits} credits to bet with this amount*`)
+
+        if (amount > 15000)
+            return void M.reply(`🟨 The maximum amount for betting is *15000*`)
+
+        const machine = new SlotMachine(3, this.symbols)
+        const results = machine.play()
+        const lines = results.lines.filter((line) => !line.diagonal)
+        const points = results.lines.reduce((total, line) => total + line.points, 0)
+
+        const resultAmount = points <= 0 ? -amount : amount
+        await this.client.DB.setGold(M.sender.jid, resultAmount)
+
+        this.betsCount++;
+
+        const thumbnailUrl = this.getRandomThumbnailUrl();
+
+        let text = '🎰 *SLOT MACHINE* 🎰\n\n'
+        text += results.visualize()
+
+        if (this.betsCount >= this.jackpotThreshold) {
+            const jackpotWin = amount * this.jackpotMultiplier;
+            text += `\n\n🎉🎉🎉 JACKPOT You won *${jackpotWin}* ! 🎉🎉🎉`;
+            this.betsCount = 0;
+            await this.client.DB.setGold(M.sender.jid, jackpotWin); 
+        } else {
+            text += points <= 0 ? `\n\n📉 You lost *${amount}*.` : `\n\n📈 You won *${resultAmount}*`;
+        }
+
+        return void (await M.reply(text, 'text', undefined, undefined, undefined, [M.sender.jid], {
+            title: this.client.utils.capitalize(`🎰𝐒𝐋𝐎𝐓 𝐌𝐀𝐂𝐇𝐈𝐍𝐄🎰`),
+            thumbnail: await this.client.utils.getBuffer(thumbnailUrl),
+            mediaType: 1
+        }));
+    }
+    }
